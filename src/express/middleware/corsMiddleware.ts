219b7bc16b0b923871d9d56f6ca035f8657fd8eb
dev2t <dev2@advcomm.ca>
2026-06-05 14:34:05 +0500

    fix(cors): allow token delivery header and PATCH/DELETE preflight
    Expose X-Uids-Token-Delivery in CORS and permit state-changing methods
    needed by SPA cookie refresh flows.

--- src/express/middleware/corsMiddleware.ts
+++ src/express/middleware/corsMiddleware.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import type { AuthKit } from "../../config.js";
+import { TOKEN_DELIVERY_HEADER } from "../../oauth/refreshCookie.js";
 import { DEVICE_ID_HEADER } from "../../types.js";
 
 export function createCorsMiddleware(kit: AuthKit) {
@@ -21,9 +22,18 @@ export function createCorsMiddleware(kit: AuthKit) {
 				res.setHeader("Vary", "Origin");
 				res.setHeader(
 					"Access-Control-Allow-Headers",
-					`Content-Type, Authorization, X-CSRF-Token, ${DEVICE_ID_HEADER}`,
+					[
+						"Content-Type",
+						"Authorization",
+						"X-CSRF-Token",
+						DEVICE_ID_HEADER,
+						TOKEN_DELIVERY_HEADER,
+					].join(", "),
 				);
-				res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+				res.setHeader(
+					"Access-Control-Allow-Methods",
+					"GET, POST, PUT, PATCH, DELETE, OPTIONS",
+				);
 			}
 		}
 
